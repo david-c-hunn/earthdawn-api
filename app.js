c0232ecb403a85a15de6a682141feb1fca91e0c6
Dave <david.c.hunn@gmail.com>
2016-10-05 21:39:48 -0700

    put a bunch of code inside the db callback

--- app.js
+++ app.js
@@ -1,56 +1,59 @@
-var express = require('express'),
-    mongoose = require('mongoose'),
+var express = require('express'), mongoose = require('mongoose'),
     bodyParser = require('body-parser');
 
-var db;
+var dbUser = process.env.DB_USER_NAME;
+var dbPassword = process.env.DB_PASSWORD;
 if (process.env.ENV == 'test') {
-    db = mongoose.connect('mongodb://localhost/earthdawn_test')
+    mongoose.connect('mongodb://localhost/earthdawn_test')
 } else {
-    db = mongoose.connect('mongodb://localhost/earthdawn');
-} 
+    mongoose.connect('mongodb://localhost/earthdawn');
+}
 
-var Talent = require('./models/talentModel');
-var Knack = require('./models/knackModel');
-var Spell = require('./models/spellModel');
-var Discipline = require('./models/disciplineModel');
-
+var db = mongoose.connection;
 var app = express();
 var port = process.env.PORT || 3000;
 port = 8080;
 
-app.use(bodyParser.urlencoded({extended:true, limit: '50mb'}));
+app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
 app.use(bodyParser.json({limit: '50mb'}));
 
-var talentRouter = require('./routes/routes.js')(Talent);
-var knackRouter = require('./routes/knackRoutes.js')(Knack, Talent);
-var spellRouter = require('./routes/routes.js')(Spell);
-var disciplineRouter = require('./routes/disciplineRoutes.js')(Discipline, Talent);
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    var Talent = require('./models/talentModel');
+    var Knack = require('./models/knackModel');
+    var Spell = require('./models/spellModel');
+    var Discipline = require('./models/disciplineModel');
+    var talentRouter = require('./routes/routes.js')(Talent);
+    var knackRouter = require('./routes/knackRoutes.js')(Knack, Talent);
+    var spellRouter = require('./routes/routes.js')(Spell);
+    var disciplineRouter =
+        require('./routes/disciplineRoutes.js')(Discipline, Talent);
 
-app.use('/earthdawn/talents', talentRouter);
-app.use('/earthdawn/knacks', knackRouter);
-app.use('/earthdawn/spells', spellRouter);
-app.use('/earthdawn/disciplines', disciplineRouter);
+    app.use('/earthdawn/talents', talentRouter);
+    app.use('/earthdawn/knacks', knackRouter);
+    app.use('/earthdawn/spells', spellRouter);
+    app.use('/earthdawn/disciplines', disciplineRouter);
 
-app.get('/', function (req, res) {
-    var msg = {
-        earthdawn: 'http://' + req.headers.host + '/earthdawn'
-    }
-    res.send(msg);
-});
+    app.get('/', function(req, res) {
+        var msg = {earthdawn: 'http://' + req.headers.host + '/earthdawn'};
+        res.send(msg);
+    });
 
-app.get('/earthdawn', function (req, res) {
-    var msg = {
-        title: 'welcome to the earthdawn api!',
-        talents: 'http://' + req.headers.host + '/earthdawn/talents',
-        knacks: 'http://' + req.headers.host + '/earthdawn/knacks',
-        spells: 'http://' + req.headers.host + '/earthdawn/spells',
-        disciplines: 'http://' + req.headers.host + '/earthdawn/disciplines'
-    }
-    res.send(msg);
-});
+    app.get('/earthdawn', function(req, res) {
+        var msg = {
+            title: 'welcome to the earthdawn api!',
+            talents: 'http://' + req.headers.host + '/earthdawn/talents',
+            knacks: 'http://' + req.headers.host + '/earthdawn/knacks',
+            spells: 'http://' + req.headers.host + '/earthdawn/spells',
+            disciplines: 'http://' + req.headers.host + '/earthdawn/disciplines'
+        };
+        res.send(msg);
+    });
 
-app.listen(port, function () {
-    console.log('gulp is running me on port: ' + port);
+    app.listen(port, function() {
+        console.log('gulp is running me on port: ' + port);
+    });
+
 });
 
 module.exports = app;
